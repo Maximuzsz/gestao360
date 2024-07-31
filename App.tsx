9a5e31719c132ae82f43fa5da017524298bac581
Paulo Roberto <61876313+Maximuzsz@users.noreply.github.com>
2024-07-31 10:27:41 -0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import Routes from './src/routes';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <StatusBar  backgroundColor="#38A69D"  />
+      <Routes/>
+    </NavigationContainer>
+  );
+}
+
---
+++ src/pages/login/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export default function Login() {
+    return (
+      <View>
+        <Text>Tela de login</Text>
+      </View>
+    )
+  
+}
